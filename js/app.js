3d91a384528fef084bdfac5545263eed6b72006e
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-06 22:57:55 -0400

    fun with variables

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// Addition
+
+var a = 3;
+var b = 10;
+// 
+a += 12;
+b += 20;
+console.log(a, b);
+
+
+//Subtraction
+
+var c = 9;
+var d = 21;
+//
+c -= 6;
+d -= 31;
+console.log(c, d);
+
+//Multiplication
+
+var e = 12;
+var f = 11;
+//
+e *= 12;
+f *= 12;
+console.log(e, f);
+
+//Quotient
+
+var g = 2;
+var h = 5;
+//
+g /= 2;
+h /= 2;
+console.log(g, h);
+
+//Increments
+
+var i = 87;
+var j = 1;
+//
+i++;
+j++;
+console.log(i, j);
+
+//Decrements
+
+var k = 4;
+var l = 10;
+//
+k--;
+l--;
+console.log(k, l);
